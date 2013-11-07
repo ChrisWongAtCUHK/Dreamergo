5e1f8e8d50fd0df82766bf92b0c6278b8528e85a
ChrisWongAtCUHK <wc38.wong@gmail.com>
2013-11-07 14:13:15 +0800

    Dreamergo

---
+++ README.md
@@ -0,0 +1,2 @@
+Dreamergo
+http://www.skypost.hk/column/%E8%8A%9D%E9%BA%BB%E7%BE%94/007001004049?m=m&pageMethod=upOrDown&currentPage=1
---
+++ dl.sh
@@ -0,0 +1,13 @@
+#
+# Load the files for proxy & download
+#
+proxy=$(head -1 proxy.txt)
+user=$(head -1 proxy.txt | tail -1)
+
+# get the urls from a text file(line by line)
+for url in $(cat urls.txt); do
+	# cut out the jpg file name
+	jpg=$(echo $url | cut -d '/' -f 9)
+	# download by curl
+	curl $url -o images\/$jpg --proxy $proxy --proxy-user $user
+done
